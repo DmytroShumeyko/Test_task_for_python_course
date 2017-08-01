d9430458b301b351bfccb35372dc2a04009d6844
Dima <dmytro.shumeyko@pramsystem.com>
2017-08-01 13:33:48 +0300

    Bitcoin address checker
    Test task for python course

---
+++ light_it.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmytro.Shumeyko
+ * Date: 8/1/2017
+ * Time: 1:20 PM
+ */
+function validateBitcoinAddress()
+{
+    //prepare array with addresses
+    $addresses = [
+        "1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i",
+        "1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62I",
+        "1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62iToLongAddress",
+        "1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62Y",
+    ];
+    //check one by one
+    foreach ($addresses as $address) {
+        $message = "Address is valid!";
+        try {
+            //call validate function
+            validate($address);
+        } catch (\Exception $e) {
+            $message = $e->getMessage();
+        }
+        echo "$address: $message\n";
+    }
+}
+
+function validate($address)
+{
+    //prepare address for hashing
+    $decoded = decode58($address);
+
+    //double hash
+    $hash1 = hash("sha256", substr($decoded, 0, 21), true);
+    $hash2 = hash("sha256", $hash1, true);
+
+    //check address
+    if (substr_compare($decoded, $hash2, 21, 4)) {
+        throw new \Exception("Invalid Bitcoin address!");
+    }
+    return true;
+}
+
+function decode58($input)
+{
+    //implement all valid chars
+    $alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+    $chars = array_fill(0, 25, 0);
+
+    for ($i = 0, $n = strlen($input); $i < $n; $i++) {
+        //check if chars is valid
+        if (($position = strpos($alphabet, $input[$i])) === false) {
+            throw new \Exception("invalid character found - '" . $input[$i] . "'");
+        }
+        //prepare address for hashing
+        $c = $position;
+        for ($j = 25; $j--;) {
+            $c += (int)(58 * $chars[$j]);
+            $chars[$j] = (int)($c % 256);
+            $c /= 256;
+            $c = (int)$c;
+        }
+        if ($c != 0) {
+            throw new \Exception("Address is too long!");
+        }
+    }
+
+    //prepare and return result
+    $result = "";
+    foreach ($chars as $val) {
+        $result .= chr($val);
+    }
+
+    return $result;
+}
+
+//run script
+validateBitcoinAddress();
